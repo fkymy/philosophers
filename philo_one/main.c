d55435ae34648a1d938d0c33e9090defd5c07e87
fkymy <yuskefuku@gmail.com>
2021-03-01 19:43:45 +0900

    permits and print status

--- philo_one/main.c
+++ philo_one/main.c
@@ -40,44 +40,80 @@ int	ft_atoi(const char *s)
 #include <stdarg.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #define SECTONSEC 1000000000 /* sec to nanosec */
 # define USECTONSEC 1000 /* microsec to nanosec */
 # define MSECTOUSEC 1000 /* milisec to microsec */
 
 const int kNumMeals = 3;
-const int no_option = 1;
+const int no_option = 0;
 
 typedef struct	s_args
 {
 	int				id;
 	pthread_mutex_t	*left;
 	pthread_mutex_t	*right;
+	int				*permits;
+	pthread_mutex_t	*permits_lock;
 }				t_args;
 
-/* int	extract_arg(t_args *a, char **argv, int option) */
-/* { */
-/*  */
-/* 	return (0); */
-/* } */
-/*  */
-/* int	arg_is_valid(int argc, char *argv[]) */
-/* { */
-/* 	return (1); */
-/* } */
-
-void	think(int id)
+long	utime(void)
 {
-	printf("%d is thinking\n", id);
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000000 + tv.tv_usec);
 }
 
-void	eat(int id, pthread_mutex_t *left, pthread_mutex_t *right)
+void	print_status(long utime, int id, char *msg)
 {
+	long	mtime;
+
+	mtime = utime / 1000;
+	printf("%ld %d %s\n", mtime, id, msg);
+}
+
+long	gettimestamp(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+void	wait_for_permission(int *permits, pthread_mutex_t *permits_lock)
+{
+	while (1)
+	{
+		pthread_mutex_lock(permits_lock);
+		if (*permits > 0)
+			break ;
+		pthread_mutex_unlock(permits_lock);
+		usleep(10);
+	}
+	(*permits)--;
+	pthread_mutex_unlock(permits_lock);
+}
+
+void	grant_permission(int *permits, pthread_mutex_t *permits_lock)
+{
+	pthread_mutex_lock(permits_lock);
+	(*permits)++;
+	pthread_mutex_unlock(permits_lock);
+}
+
+void	eat(int id, pthread_mutex_t *left, pthread_mutex_t *right, int *permits, pthread_mutex_t *permits_lock)
+{
+	wait_for_permission(permits, permits_lock);
 	pthread_mutex_lock(left);
-	usleep(1000 * MSECTOUSEC);
+	print_status(utime(), id, "has taken a fork");
+	// usleep(1000 * MSECTOUSEC); // for deadlock check
 	pthread_mutex_lock(right);
-	printf("%d is eating\n", id);
-	usleep(2000 * MSECTOUSEC);
+	print_status(utime(), id, "has taken a fork");
+	print_status(utime(), id, "is eating");
+	usleep(1000 * MSECTOUSEC);
+	grant_permission(permits, permits_lock);
 	pthread_mutex_unlock(left);
 	pthread_mutex_unlock(right);
 }
@@ -87,18 +123,17 @@ void	*philosopher(void *args)
 	t_args *a;
 
 	a = (t_args *)args;
-	printf("thread started: %d\n", a->id);
-	// while number of meals to eat, eat, sleep, think
+	print_status(utime(), a->id, "thread started");
 	int i = 0;
 	while (i < kNumMeals || no_option)
 	{
-		eat(a->id, a->left, a->right);
-		printf("%d is sleeping\n", a->id);
-		usleep(3000 * MSECTOUSEC);
-		printf("%d is thinking\n", a->id);
+		eat(a->id, a->left, a->right, a->permits, a->permits_lock);
+		print_status(utime(), a->id, "is sleeping");
+		usleep(1000 * MSECTOUSEC);
+		print_status(utime(), a->id, "is thinking");
 		i++;
 	}
-	return (NULL);
+	return ((void *)a);
 }
 
 int main(int argc, char *argv[])
@@ -109,15 +144,17 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	/* max number of threads? */
 	int num_philo = ft_atoi(argv[1]);
-	if (num_philo <= 0)
+	if (num_philo <= 1)
 	{
 		printf("Error: Invalid arguments\n");
 		return (1);
 	}
 	printf("num_philo: %d\n", num_philo);
 
+	int permits = num_philo - 1;
+	pthread_mutex_t permits_lock;
+	/* pthread_mutex_t death_lock; */
 	pthread_mutex_t forks[num_philo];
 	pthread_t philos[num_philo];
 	t_args args[num_philo];
@@ -131,13 +168,18 @@ int main(int argc, char *argv[])
 		args[i].id = i;
 		args[i].left = &forks[i];
 		args[i].right = &forks[(i + 1) % num_philo];
+		args[i].permits = &permits;
+		args[i].permits_lock = &permits_lock;
 		pthread_create(&philos[i], NULL, philosopher, (void *)(args + i));
 		i++;
 	}
+
+	t_args *ret;
 	int j = 0;
 	while (j < num_philo)
 	{
-		pthread_join(philos[j], NULL);
+		pthread_join(philos[j], (void *)&ret);
+		printf("ret! j: %d, id: %d\n", j, ret->id);
 		j++;
 	}
 	return (0);
