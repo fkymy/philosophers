7f9bf47f36cd18f2b7535ec580c83a0c85d5bbf5
fkymy <yuskefuku@gmail.com>
2021-03-09 15:20:17 +0900

    bbuffer

--- misc/bbuffer.c
+++ misc/bbuffer.c
@@ -1,7 +1,3 @@
-/**
- * Circular bounded buffer with mutual exclusion
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -9,9 +5,12 @@
 #include <pthread.h>
 
 #define BBUF_SIZE 128
-#define min(a, b) ((a < b) ? a : b)
+#define min(a,b) ({\
+		__typeof__ (a) _a = (a); \
+		__typeof__ (b) _b = (b); \
+		_a < _b ? _a : _b; })
 
-struct bbuffer {
+struct bbuf {
 	char buf[BBUF_SIZE];
 	size_t capacity;
 	pthread_mutex_t	lock;
@@ -20,11 +19,11 @@ struct bbuffer {
 	int write_closed;
 };
 
-struct bbuffer *bbuf_init(void)
+struct bbuf *bbuf_init(void)
 {
-	struct bbuffer *b;
+	struct bbuf *b;
 
-	b = malloc(sizeof(struct bbuffer));
+	b = malloc(sizeof(struct bbuf));
 	b->capacity = BBUF_SIZE;
 	pthread_mutex_init(&b->lock, NULL);
 	b->pos = 0;
@@ -33,40 +32,46 @@ struct bbuffer *bbuf_init(void)
 	return (b);
 };
 
-ssize_t bbuf_write(struct bbuffer *b, const char *buf, size_t sz)
+void bbuf_destory(struct bbuf *b)
+{
+	pthread_mutex_destroy(&b->lock);
+	/* shutdown write... */
+}
+
+ssize_t bbuf_write(struct bbuf *b, const char *buf, size_t size)
 {
 	pthread_mutex_lock(&b->lock);
 	assert(!b->write_closed);
 	size_t pos = 0;
-	while (pos < sz && b->len < b->capacity) {
+	while (pos < size && b->len < b->capacity) {
 		size_t bindex = (b->pos + b->len) % b->capacity;
 		size_t bspace = min(b->capacity - bindex, b->capacity - b->len);
-		size_t n = min(sz - pos, bspace);
+		size_t n = min(size - pos, bspace);
 		memcpy(&b->buf[bindex], &buf[pos], n);
 		b->len += n;
 		pos += n;
 	}
 	pthread_mutex_unlock(&b->lock);
-	if (pos == 0 && sz > 0) {
+	if (pos == 0 && size > 0) {
 		return -1;
 	} else {
 		return pos;
 	}
 }
 
-ssize_t bbuf_read(struct bbuffer *b, const char *buf, size_t sz)
+ssize_t bbuf_read(struct bbuf *b, char *buf, size_t size)
 {
-	size_t pos = 0;
 	pthread_mutex_lock(&b->lock);
-	while (pos < sz && b->len > 0) {
+	size_t pos = 0;
+	while (pos < size && b->len > 0) {
 		size_t bspace = min(b->len, b->capacity - b->pos);
-		size_t n = min(sz - pos, bspace);
+		size_t n = min(size - pos, bspace);
 		memcpy(&buf[pos], &b->buf[b->pos], n);
 		b->pos = (b->pos + n) % b->capacity;
 		b->len -= n;
 		pos += n;
 	}
-	if (pos == 0 && sz > 0 && !b->write_closed) {
+	if (pos == 0 && size > 0 && !b->write_closed) {
 		pos = -1;
 	}
 	pthread_mutex_unlock(&b->lock);
---
+++ misc/condv-buffer.c
@@ -0,0 +1,103 @@
+
+/*
+ * condv-buffer.c -- 条件変数を使った環状バッファ
+ */
+
+#include <stdio.h>	/* printf() */
+#include <stdlib.h>	/* malloc(), exit() */
+#include <pthread.h>
+
+#define	BUFFER_SIZE	4		/* バッファの大きさ */
+struct circular_buffer
+{
+	int rp ;			/* 読み出す位置 */
+	int wp ;			/* 書き込む位置 */
+    	int used ;			/* バッファ内の要素数 */
+	int data[BUFFER_SIZE];		/* データを保存する場所 */
+	pthread_mutex_t	mutex ;		/* この構造体の相互排除のための mutex */
+	pthread_cond_t	not_full ;	/* バッファが一杯ではない状態を待つための条件変数 */
+	pthread_cond_t	not_empty ;	/* バッファが空ではない状態を待つための条件変数 */
+};
+
+void thread_A(struct circular_buffer *b);
+void thread_B(struct circular_buffer *b);
+
+void put( struct circular_buffer *b,int x )
+{
+	pthread_mutex_lock( &b->mutex );
+loop:	if( b->used == BUFFER_SIZE )
+	{
+	    pthread_cond_wait( &b->not_full,&b->mutex );
+	    goto loop;
+	}
+	b->data[ b->wp++ ] = x ;
+	if( b->wp >= BUFFER_SIZE )
+	    b->wp = 0 ;
+	b->used ++ ;
+	pthread_cond_broadcast( &b->not_empty );
+	pthread_mutex_unlock( &b->mutex );
+}
+
+int get( struct circular_buffer *b )
+{
+    int x ;
+	pthread_mutex_lock( &b->mutex );
+loop:	if( b->used == 0 )
+	{
+	    pthread_cond_wait( &b->not_empty,&b->mutex );
+	    goto loop;
+	}
+	x = b->data[ b->rp++ ] ;
+	if( b->rp >= BUFFER_SIZE )
+	    b->rp = 0 ;
+	b->used -- ;
+	pthread_cond_broadcast( &b->not_full );
+	pthread_mutex_unlock( &b->mutex );
+	return( x );
+}
+
+int main()
+{
+    pthread_t t1 ;
+    pthread_t t2 ;
+    struct circular_buffer *b  ;
+	b = (struct circular_buffer *)malloc(sizeof(struct circular_buffer));
+    	if( b == NULL )
+	{
+	    perror("no memory for struct buffer\n");
+	    exit( -1 );
+	}
+	b->rp = 0 ;
+	b->wp = 0 ;
+	b->used = 0 ;
+	pthread_mutex_init( &b->mutex, NULL );
+	pthread_cond_init( &b->not_full,NULL );
+	pthread_cond_init( &b->not_empty,NULL );
+	pthread_setconcurrency( 2 );
+	pthread_create( &t1, NULL, (void *)thread_A, (void *)b );
+	pthread_create( &t2, NULL, (void *)thread_B, (void *)b );
+	pthread_join( t1, NULL );
+	pthread_join( t2, NULL );
+	return( 0 );
+}
+
+void thread_A( struct circular_buffer *b )	/* producer */
+{
+    int i,x ;
+	for( i = 0 ; i<10 ; i++ )
+	{
+	    x = i ;
+	    printf("thread_A(): put( %d )\n",x );
+	    put( b,x );
+	}
+}
+
+void thread_B( struct circular_buffer *b )	/* consumer */
+{
+    int i, x ;
+	for( i = 0 ; i<10 ; i++ )
+	{
+	    x = get( b );
+	    printf("thread_B(): get() %d.\n", x );
+	}
+}
